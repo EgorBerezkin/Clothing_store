4b44dc799e3845d3ca97788613eaf04bc85faa4f
st403 <st403@WS403-05>
2026-02-16 08:53:54 +0300

    создал папку Model, в которой добавил классы EFModel, Buyer, Clothes.

---
+++ Model/Clothes.cs
@@ -0,0 +1,12 @@
+﻿namespace Magazin_odejdi.Model
+{
+    public class Clothes : EFModel
+    {
+        public string Naimenovanie { get; set; }
+        public string Category { get; set; }
+        public string Size { get; set; } // размер
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Model/EFModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Magazin_odejdi.Model
+{
+    public class EFModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
