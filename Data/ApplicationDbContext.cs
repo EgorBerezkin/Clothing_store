cb18cc370a5751549cd538cb5592ca685a676613
st403 <st403@WS403-05>
2026-02-16 09:33:23 +0300

    Настроил подключение к базе данных

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Magazin_odejdi.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Magazin_odejdi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+            //Database.Migrate();
+        }
+
+        public DbSet<Buyer> Buyers { get; set; }
+        public DbSet<Clothes> Clothess { get; set; }
+
+    }
+}
