e0ab7627d90fc79fa3f840dae0bcaba57dee15b3
st403 <st403@WS403-05>
2026-04-20 08:56:53 +0300

    добавил выпадание ошибки при незаполнении полей, реализовал темную тему

--- Model/Buyer.cs
+++ Model/Buyer.cs
@@ -1,10 +1,19 @@
-﻿namespace Magazin_odejdi.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Magazin_odejdi.Model
 {
     public class Buyer : EFModel // посетитель
     {
+        [Required(ErrorMessage = "Пожалуйста, заполните ФИО")]
         public string? FIO { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, заполните поле телефон")]
         public string? Telefon { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, заполните поле E-mail")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, заполните поле дата рождения")]
         public DateTime Data_BirthDay {  get; set; }
 
         /*
--- Model/Clothes.cs
+++ Model/Clothes.cs
@@ -1,7 +1,10 @@
-﻿namespace Magazin_odejdi.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Magazin_odejdi.Model
 {
     public class Clothes : EFModel
     {
+        [Required(ErrorMessage = "Пожалуйста, заполните поле имя")]
         public string? Naimenovanie { get; set; }
         public string? Category { get; set; }
         public string? Size { get; set; } // размер одежды
