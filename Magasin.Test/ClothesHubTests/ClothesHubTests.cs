e33cc68dfb73faacfa6d8b45d46d30fc58db8668
Fa1tloy <baranovdaniil534@gmail.com>
2026-04-25 14:15:18 +0300

    сделал тест с hub

---
+++ Magasin.Test/ClothesHubTests/ClothesHubTests.cs
@@ -0,0 +1,52 @@
+﻿using Magazin_odejdi.Model;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magazin_odejdi.Hubs;
+using Moq;
+
+namespace Magasin.Test.ClothesHubTests.Hubs
+{
+    public class ClothesHubTests
+    {
+        [Fact]
+        public async Task SendClothesUpdate_ShouldSendMessageToAllClients()
+        {
+            // Arrange
+            var hub = new ClothesHub();
+
+            var clientsMock = new Mock<IHubCallerClients>();
+            var clientProxyMock = new Mock<IClientProxy>();
+
+            clientsMock.Setup(c => c.All).Returns(clientProxyMock.Object);
+
+            hub.Clients = clientsMock.Object;
+
+            var clothes = new Clothes
+            {
+                Naimenovanie = "Лонгслив с надписью",
+                Category = "Вверхняя одежда",
+                Size = "XL",
+                Color = "Коричневый",
+                Material = "Хлопок",
+                Price = 1249
+            };
+
+            // Act
+            await hub.SendClothesUpdate(clothes);
+
+            // Assert
+            clientProxyMock.Verify(
+                c => c.SendCoreAsync(
+                    "ClothesUpdated",
+                    It.Is<object[]>(o => o.Length == 1 && o[0] == clothes),
+                    default
+                ),
+                Times.Once
+            );
+        }
+    }
+}
---
+++ Magazin_odejdi/Hubs/ClothesHub.cs
@@ -0,0 +1,14 @@
+﻿using Magazin_odejdi.Model;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Magazin_odejdi.Hubs
+{
+    public class ClothesHub : Hub
+    {
+        // Отправка обновления книги всем клиентам
+        public async Task SendClothesUpdate(Clothes clothes)
+        {
+            await Clients.All.SendAsync("ClothesUpdated", clothes);
+        }
+    }
+}
