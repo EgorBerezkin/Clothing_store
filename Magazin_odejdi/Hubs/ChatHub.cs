c56bf00cada6047d8eaad167534adb44714696e0
Egor Briggs <berezkin07egor@gmail.com>
2026-07-01 22:12:08 +0300

    Исправил чат, чтобы в темной теме было видно сообщение

--- Magazin_odejdi/Hubs/ChatHub.cs
+++ Magazin_odejdi/Hubs/ChatHub.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Magazin_odejdi.Hubs
 {
+    [Authorize]
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
+            string role = Context.User?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value ?? "User";
+            string time = DateTime.Now.ToString("HH:mm");
+
             await Clients.All.SendAsync("Receive", user, message);
         }
     }
