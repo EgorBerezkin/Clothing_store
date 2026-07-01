44b1ee1ae54330d44e5dad36da5ce0f41015b1be
Egor Briggs <berezkin07egor@gmail.com>
2026-07-01 22:27:23 +0300

    исправил чат

--- Magazin_odejdi/Hubs/ChatHub.cs
+++ Magazin_odejdi/Hubs/ChatHub.cs
@@ -9,9 +9,6 @@ namespace Magazin_odejdi.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
-            string role = Context.User?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value ?? "User";
-            string time = DateTime.Now.ToString("HH:mm");
-
             await Clients.All.SendAsync("Receive", user, message);
         }
     }
